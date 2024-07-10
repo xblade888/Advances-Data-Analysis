7fbea994b88921406edb2ec12ee64c85091f8169
xblade888 <thomas.thierauf@gmail.com>
2024-07-10 14:31:24 +0200

    Update Vorverarbeitung_Lemmatisierung_Stoppwörter.py

--- Vorverarbeitung_Lemmatisierung_Stoppwörter.py
+++ Vorverarbeitung_Lemmatisierung_Stoppwörter.py
@@ -1,3 +1,7 @@
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import nltk
+
+bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
+lemmatisierung = WordNetLemmatizer()
+stoppwoerter = set(stopwords.words('english'))

11b26a21f4e3ccb7b3b865cf9bea9a0662511fb5
xblade888 <thomas.thierauf@gmail.com>
2024-07-10 14:21:20 +0200

    Update Vorverarbeitung_Pandas_re.py

--- Vorverarbeitung_Pandas_re.py
+++ Vorverarbeitung_Pandas_re.py
@@ -3,8 +3,9 @@ import re
 
 bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
 
-def bereinigung(bewertungen):
-  bewertungen = bewertungen.lower()
-  bewertungen = re.sub(r'[A-Za-z0-9üäöÜÄÖ], '', bewertungen)
-  bewertungen = re.sub(r'http\S+', '', stringliteral)
-  return bewertungen
+def bereinigen(text):
+    text = text.lower()
+    text = re.sub(r'[^A-Za-z\süäöÜÄÖ]', '', text)
+    text = re.sub(r'http\S+', '', text)
+    return text
+bewertungen['review_text'] = bewertungen['review_text'].apply(bereinigen)

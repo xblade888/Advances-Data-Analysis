0e0fb5c6fad950c0fd18b6670f11f34920df920b
xblade888 <thomas.thierauf@gmail.com>
2024-07-10 14:32:32 +0200

    Update Vorverarbeitung_Lemmatisierung_Stoppwörter.py

--- Vorverarbeitung_Lemmatisierung_Stoppwörter.py
+++ Vorverarbeitung_Lemmatisierung_Stoppwörter.py
@@ -5,3 +5,9 @@ import nltk
 bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
 lemmatisierung = WordNetLemmatizer()
 stoppwoerter = set(stopwords.words('english'))
+
+def lemmatisierung_und_stoppwörter_entfernen(text):
+    woerter = text.split()
+    bereinigte_woerter = [lemmatisierer.lemmatize(wort) for wort in woerter if wort not in stoppwoerter]
+    return ' '.join(bereinigte_woerter)
+

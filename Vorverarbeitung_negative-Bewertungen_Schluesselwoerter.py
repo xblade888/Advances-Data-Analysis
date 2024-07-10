626b48e37d59bad9a1740abe96b3698669b9278a
xblade888 <thomas.thierauf@gmail.com>
2024-07-10 13:39:50 +0200

    Update Vorverarbeitung_negative-Bewertungen_Schluesselwoerter.py

--- Vorverarbeitung_negative-Bewertungen_Schluesselwoerter.py
+++ Vorverarbeitung_negative-Bewertungen_Schluesselwoerter.py
@@ -1,12 +1,12 @@
 import pandas as pd
-bewertungen = pd.read_csv('C:/temp/studium/TINDER_REViEWS.csv')
+bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
 
-bewertungen = bewertungen[bewertungen['review_rating'].isin(1,2)]
+bewertungen = bewertungen[bewertungen['review_rating'].isin([1, 2])]
 
 schluesselwoerter = ['slow', 'lag', 'crash', 'freeze', 'bug', 'error', 'glitch', 'problem', 'fail', 'broken', 'frustrating', 'disappointing', 'poor', 'unusable',     'not working', 'stupid', 'bad', 'not loading', 'not working', 'connection issues', 'can not access', 'confusing', 'hard to use', 'difficult']
 
 pattern = '|'.join(schluesselwoerter)
-bewertungen = bewertungen[bewertungen['review_text'].str.containts(pattern, case=False, na=False)]
+bewertungen = bewertungen[bewertungen['review_text'].str.contains(pattern, case=False, na=False)]
 
 bewertungen.to_csv('C:/temp/studium/TINDER_REViEWS.csv', index=False)
-print("Neue Datei gespeichert")
+print("Bewertungen nach Schluesselwoerter gefiltert, Datei überschrieben")

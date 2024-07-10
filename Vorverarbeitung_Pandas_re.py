b57c549e0b2e24d1dd85abaf5120f10c79f68667
xblade888 <thomas.thierauf@gmail.com>
2024-07-10 14:22:10 +0200

    Update Vorverarbeitung_Pandas_re.py

--- Vorverarbeitung_Pandas_re.py
+++ Vorverarbeitung_Pandas_re.py
@@ -9,3 +9,9 @@ def bereinigen(text):
     text = re.sub(r'http\S+', '', text)
     return text
 bewertungen['review_text'] = bewertungen['review_text'].apply(bereinigen)
+
+bewertungen.dropna(subset=['review_text'], inplace=True)
+bewertungen.drop_duplicates(subset=['review_text'], inplace=True)
+
+bewertungen.to_csv('C:/temp/studium/TINDER_REVIEWS.csv', index=False)
+print("Bewertungen erfolgreich gefiltert, Datei überschrieben")

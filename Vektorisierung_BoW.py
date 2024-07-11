779a6278cf8f5c7216ef12789d184f2d4ca7934f
xblade888 <thomas.thierauf@gmail.com>
2024-07-11 10:26:18 +0200

    Update Vektorisierung_BoW.py

--- Vektorisierung_BoW.py
+++ Vektorisierung_BoW.py
@@ -7,8 +7,8 @@ bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
 vectorizer = CountVectorizer()
 bow_matrix = vectorizer.fit_transform(bewertungen['review_text'])
 
-bow_file = 'C:/temp/studium/BoW_cleaned_tinder_reviews.npz'
-np.savez_compressed(sparse_matrix_file, data=bow_matrix.data, indices=bow_matrix.indices, indptr=bow_matrix.indptr, shape=bow_matrix.shape)
+bow_file = 'C:/temp/studium/BoW_tinder_reviews.npz'
+np.savez_compressed(bow_file, data=bow_matrix.data, indices=bow_matrix.indices, indptr=bow_matrix.indptr, shape=bow_matrix.shape)
 print(f"Vektorisierung mit Bag-of-Words abgeschlossen. Datei: {bow_file}")
 
-print("feritg")
+print("Vektorisierierung mit Bag-of-Words abgeschlossen. In der neuen Datei BoW_tinder_reviews.npz gespeichert")

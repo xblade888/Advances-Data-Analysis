754537f6cc3cbaf7ec3ef6390efe1e893ddd10ae
xblade888 <thomas.thierauf@gmail.com>
2024-07-11 14:31:26 +0200

    Create Topic-Modelling_LSA.py

---
+++ Topic-Modelling_LSA.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import scipy.sparse
+from sklearn.decomposition import TruncatedSVD
+
+# Nutzt die BoW Vektorisierung
+file = 'C:/temp/studium/BoW_tinder_reviews.npz'

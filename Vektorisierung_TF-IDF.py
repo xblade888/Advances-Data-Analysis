e58a7150a892d9fc97ded2a80e5ef154c4770d53
xblade888 <thomas.thierauf@gmail.com>
2024-07-11 09:19:02 +0200

    Update Vektorisierung_TF-IDF.py

--- Vektorisierung_TF-IDF.py
+++ Vektorisierung_TF-IDF.py
@@ -1,1 +1,12 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 
+bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
+
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(bewertungen['review_text'])
+
+tfidf_file = 'C:/temp/studium/TFIDF_tinder_reviews.npz'
+
+print("done")

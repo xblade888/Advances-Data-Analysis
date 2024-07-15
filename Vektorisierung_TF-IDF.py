daed046d3a119b5eeb883bad3863b408cdcb30f2
xblade888 <thomas.thierauf@gmail.com>
2024-07-15 11:09:25 +0200

    Update Vektorisierung_TF-IDF.py

--- Vektorisierung_TF-IDF.py
+++ Vektorisierung_TF-IDF.py
@@ -11,5 +11,10 @@ tfidf_matrix = vectorizer.fit_transform(bewertungen['review_text'])
 tfidf_file = 'C:/temp/studium/TFIDF_tinder_reviews.npz'
 scipy.sparse.save_npz(tfidf_file, tfidf_matrix)
 
+features_file = 'C:/temp/studium/TFIDF_features.txt'
+with open(features_file, 'w') as f:
+    for feature in vectorizer.get_feature_names_out():
+        f.write(f"{feature}\n")
+
 print("Vektorisierierung mit TF-IDF abgeschlossen. In der neuen Datei TFIDF_tinder_reviews.npz gespeichert")
 

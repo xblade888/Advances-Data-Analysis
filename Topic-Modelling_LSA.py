0f898d0c210fc771e1aee6b2fec95e0197d92efd
xblade888 <thomas.thierauf@gmail.com>
2024-07-15 10:57:20 +0200

    Update Topic-Modelling_LSA.py

--- Topic-Modelling_LSA.py
+++ Topic-Modelling_LSA.py
@@ -1,7 +1,25 @@
-import pandas as pd
 import numpy as np
 import scipy.sparse
 from sklearn.decomposition import TruncatedSVD
+import pandas as pd
 
-# Nutzt die BoW Vektorisierung
-file = 'C:/temp/studium/BoW_tinder_reviews.npz'
+BoW_reviews = 'C:/temp/studium/BoW_tinder_reviews.npz'
+matrix = scipy.sparse.load_npz(BoW_reviews)
+
+BoW_features = 'C:/temp/studium/BoW_features.txt'
+with open(BoW_features, 'r') as f:
+    terms = np.array([line.strip() for line in f])
+
+# 15 Themen entdecken
+n_components = 15 
+modell = TruncatedSVD(n_components=n_components, random_state=42)
+matrix = modell.fit_transform(matrix)
+
+print("LSA Themen:")
+for i, comp in enumerate(modell.components_):
+    terms_in_comp = terms[np.argsort(comp)][:-16:-1]  # pro Thema 15 Wörter 
+    print(f"Topic {i}: {', '.join(terms_in_comp)}")
+
+lsa = 'C:/temp/studium/LSA_themen.npy'
+np.save(lsa, matrix)
+print("Tpic-Modelelling mit LSA abgeschlossen. In der neuen Datei LSA_themen.npy gespeichert")

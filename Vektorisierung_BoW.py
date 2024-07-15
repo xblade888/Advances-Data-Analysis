2f812247dc94cf8478efaef7def339bb5cad4c9f
xblade888 <thomas.thierauf@gmail.com>
2024-07-15 10:44:01 +0200

    Update Vektorisierung_BoW.py

--- Vektorisierung_BoW.py
+++ Vektorisierung_BoW.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+import scipy.sparse
 import numpy as np
 
 bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
@@ -8,7 +9,6 @@ vectorizer = CountVectorizer()
 bow_matrix = vectorizer.fit_transform(bewertungen['review_text'])
 
 bow_file = 'C:/temp/studium/BoW_tinder_reviews.npz'
-np.savez_compressed(bow_file, data=bow_matrix.data, indices=bow_matrix.indices, indptr=bow_matrix.indptr, shape=bow_matrix.shape)
-print(f"Vektorisierung mit Bag-of-Words abgeschlossen. Datei: {bow_file}")
+scipy.sparse.save_npz(bow_file, bow_matrix)
 
 print("Vektorisierierung mit Bag-of-Words abgeschlossen. In der neuen Datei BoW_tinder_reviews.npz gespeichert")

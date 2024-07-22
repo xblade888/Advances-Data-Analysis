94bc377aa97105592d2f153a99b5c7c12e074704
xblade888 <thomas.thierauf@gmail.com>
2024-07-22 10:22:55 +0200

    Update Coherence-Score_LDA.py

--- Coherence-Score_LDA.py
+++ Coherence-Score_LDA.py
@@ -3,22 +3,27 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-lda_file = 'C:/temp/studium/LDA_themen.npy'
-matrix = np.load(lda_file)
+# Laden des LSA-Modeslls  aus dem vorherigen Schritt + Laden den bereinigten Bewertungsdatei 
+lda = 'C:/temp/studium/LDA_themen.npy'
+matrix = np.load(lda)
 bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
 
+# Laden des Vectoriziers
 vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(bewertungen['review_text'])
+term = vectorizer.fit_transform(bewertungen['review_text'])
 
+# Laden der Feature-Wörter aus dem vorherigen Schritt
 features = 'C:/temp/studium/TFIDF_features.txt'
 with open(features, 'r') as f:
     terms = [line.strip() for line in f]
 
+# Speichern des Hautptwortes jedes Thema aus der LSA-Matrix
 topics = []
 for topic_idx in range(matrix.shape[0]):
     topic_terms = [terms[i] for i in np.argsort(matrix[topic_idx])[::-1][:15] if i < len(terms)]
     topics.append(topic_terms)
 
+# Speichern in einer Textdatatei uns Ausgabe in der CLI
 file = 'C:/temp/studium/LDA_topics.txt'
 with open(file, 'w') as f:
     for idx, topic in enumerate(topics):
@@ -28,7 +33,8 @@ print("LDA Themen:")
 for idx, topic in enumerate(topics):
     print(f"Topic {idx}: {', '.join(topic)}")
 
-def calculate_coherence(topics, vectorizer, X):
+# Funktion für den Coherence Score mit Rückgabe an das Hauptprogramm
+def coherence(topics, vectorizer, X):
     coherence_scores = []
     for topic in topics:
         topic_indices = [vectorizer.vocabulary_.get(term) for term in topic if term in vectorizer.vocabulary_]
@@ -40,5 +46,6 @@ def calculate_coherence(topics, vectorizer, X):
         coherence_scores.append(coherence)
     return np.mean(coherence_scores)
 
-lda_coherence_score = calculate_coherence(topics, vectorizer, X)
+# Berechnung des durschschnittlichen Scores uns Ausgabe in der CLI
+lda_coherence_score = coherence(topics, vectorizer, term)
 print(f'Coherence Score für LDA: {lda_coherence_score}')

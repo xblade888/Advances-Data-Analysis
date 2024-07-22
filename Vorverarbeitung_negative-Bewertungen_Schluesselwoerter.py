838efbd4ca0052f9fcf6d9e1abe726ca82e6ac24
xblade888 <thomas.thierauf@gmail.com>
2024-07-22 09:25:28 +0200

    Update Vorverarbeitung_negative-Bewertungen_Schluesselwoerter.py

--- Vorverarbeitung_negative-Bewertungen_Schluesselwoerter.py
+++ Vorverarbeitung_negative-Bewertungen_Schluesselwoerter.py
@@ -1,12 +1,19 @@
 import pandas as pd
+
+# Datensatz laden (als CSV heruntergeladen)
 bewertungen = pd.read_csv('C:/temp/studium/TINDER_REVIEWS.csv')
 
+# Filtern der Bewertungen nach 1+2 Sterne
 bewertungen = bewertungen[bewertungen['review_rating'].isin([1, 2])]
 
+# Schlüsselwörter nach denen gefiltert wird (Onlinerecherche)
 schluesselwoerter = ['slow', 'lag', 'crash', 'freeze', 'bug', 'error', 'glitch', 'problem', 'fail', 'broken', 'frustrating', 'disappointing', 'poor', 'unusable',     'not working', 'stupid', 'bad', 'not loading', 'not working', 'connection issues', 'can not access', 'confusing', 'hard to use', 'difficult']
 
+# Regex nach verschiedene Schlüsselwörter (ODER)
 pattern = '|'.join(schluesselwoerter)
+# Filtern nach Schlüsselwörtern, Groß- und Keinschreibung ergal
 bewertungen = bewertungen[bewertungen['review_text'].str.contains(pattern, case=False, na=False)]
 
-bewertungen.to_csv('C:/temp/studium/TINDER_REViEWS.csv', index=False)
+# Überschreiben der vorhandenen Datein und Erfolgsnachricht
+bewertungen.to_csv('C:/temp/studium/TINDER_REVIEWS.csv', index=False)
 print("Bewertungen nach Schluesselwoerter gefiltert, Datei überschrieben")
